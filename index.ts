437254e808f3ea363ec88b0c232d082ba9cbe12c
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-08-17 01:45:23 +0300

    pre release

---
+++ index.ts
@@ -0,0 +1,12 @@
+import hljs from 'highlight.js/lib/core'
+import bash from 'highlight.js/lib/languages/bash'
+import 'highlight.js/styles/dark.css'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(hljs)
+    hljs.registerLanguage('bash', bash)
+    document.querySelectorAll('code').forEach((block) => {
+        hljs.highlightElement(block)
+    })
+})
