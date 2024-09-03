dbfca4a5dc99b96769741e2b077b4b3e3b59e6f4
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-09-03 13:56:14 +0300

    build

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js/lib/core'
 import bash from 'highlight.js/lib/languages/bash'
 import 'highlight.js/styles/dark.css'
+import './src/index.css'
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,4 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const video = document.getElementById('video') as HTMLVideoElement
     video.play()
+
+    video.addEventListener('loadeddata', () => {
+        console.log('loaded')
+        const vidCont = document.getElementById('vid-cont') as HTMLDivElement
+        const prog = document.getElementById('prog') as HTMLDivElement
+
+        vidCont.style.opacity = "1"
+        prog.style.display = 'none'
+    })
+
 })
