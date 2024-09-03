d0ce51fece4585ae05b67aceca8d7370aa69b687
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-09-03 15:08:31 +0300

    build

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded')
     const vidCont = document.getElementById('vid-cont') as HTMLDivElement
     const video = document.getElementById('video') as HTMLVideoElement
+    const vidSrc = document.getElementById('vid-src') as HTMLSourceElement
     const prog = document.getElementById('prog') as HTMLDivElement
 
     function hideProg() {
@@ -17,8 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function progress() {
-        console.log('progress')
         const percent = (video.buffered.end(0) / video.duration * 100).toFixed(2)
+        console.log('progress')
+        console.log(percent)
         prog.innerText = `${percent}%`
         prog.style.width = `${video.buffered.end(0) / video.duration * 100 * 630}px`
     }
@@ -31,4 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('code').forEach((block) => {
         hljs.highlightElement(block)
     })
+
+    vidSrc.src = 'video.mp4'
 })
